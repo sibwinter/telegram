6380bac0375612b77b2aa46cb06e6f7e15f63928
Denis Seredin <sibwinter42@gmail.com>
2023-01-09 14:49:50 +0300

    testing requests

--- some_file.py
+++ some_file.py
@@ -1,1 +1,9 @@
-print("test ok")
+import requests
+
+API_URL = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(API_URL)
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(response.status_code)
